b74f01df1a1c4a1ff0a6b99edd46b424906176ea
niijima16 <niijima.16@gamil.com>
2024-10-20 18:46:45 +0900

    Middlware finished 1020T1846

--- CORS_models/main.py
+++ CORS_models/main.py
@@ -1,17 +1,36 @@
 import uvicorn
-from fastapi import FastAPI
-
-
+from fastapi import FastAPI, Request
+from fastapi.responses import Response
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
+# @app.middleware("http")
+# async def TestCORS(request:Request,call_next):
+#     response = await call_next(request)
+#     response.headers["Acess-Control-Allow-Orrgin"] = "*"
+    
+#     return response
+
+origins = [
+    "http://localhost:8060"
+]
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/user")
 def get_user():
+    print("come from outside")
     return {
-        "user":"current user"
+        "user":"Skadi"
     }
+
     
 
     
